7ab55101434af494b84580b817d1262a99be69ed
Prasanna Gautam <prasanna@anchorlabs.com>
2025-11-04 00:24:42 +0000

    remove the IERC20 changes that are addressed in #29

--- src/chain_parsers/visualsign-ethereum/src/lib.rs
+++ src/chain_parsers/visualsign-ethereum/src/lib.rs
@@ -1,7 +1,6 @@
 use alloy_consensus::{Transaction as _, TxType, TypedTransaction};
 use alloy_primitives::{U256, utils::format_units};
 use alloy_rlp::{Buf, Decodable};
-use alloy_sol_types::sol;
 use base64::{Engine as _, engine::general_purpose::STANDARD as b64};
 use visualsign::{
     SignablePayload, SignablePayloadField, SignablePayloadFieldCommon, SignablePayloadFieldTextV2,
@@ -43,24 +42,6 @@ fn format_ether(wei: U256) -> String {
 fn format_gwei(wei: u128) -> String {
     let wei_u256 = alloy_primitives::U256::from(wei);
     trim_trailing_zeros(format_units(wei_u256, "gwei").unwrap_or_else(|_| wei.to_string()))
-}
-
-sol! {
-    #[sol(rpc)]
-    interface IERC20 {
-        function name() external view returns (string memory);
-        function symbol() external view returns (string memory);
-        function decimals() external view returns (uint8);
-        function totalSupply() external view returns (uint256);
-        function balanceOf(address account) external view returns (uint256);
-        function transfer(address to, uint256 amount) external returns (bool);
-        function allowance(address owner, address spender) external view returns (uint256);
-        function approve(address spender, uint256 amount) external returns (bool);
-        function transferFrom(address from, address to, uint256 amount) external returns (bool);
-
-        event Transfer(address indexed from, address indexed to, uint256 value);
-        event Approval(address indexed owner, address indexed spender, uint256 value);
-    }
 }
 
 /// Wrapper around Alloy's transaction type that implements the Transaction trait
